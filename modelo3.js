26f324aa025aa225bf7603433d3ccf97d5e523ac
Thiago Pires <104836232+ThiagoNome@users.noreply.github.com>
2023-02-06 09:52:01 -0300

    atualização 2

--- modelo3.js
+++ modelo3.js
@@ -1,8 +1,8 @@
 let registro = prompt("Deseja se registrar? \n 1-Sim \n 2-Não");
 let experiencia_m = ""
-let idade = Number(0);
+let idade = 0;
 let mIdade = Number(0);
-let totalIdade = Number(0);
+let totalIdade = 0;
 let totalCandidatos = 0;
 let totalExpe = 0;
 let msg = "";
@@ -10,12 +10,15 @@ let contIdade = 0;
 while(registro === "1"){
     let num_candidato = prompt ("Digite o número do candidato.");
     let nome = prompt("Digite o seu nome:");
-    idade = prompt("Digite a sua idade:");
+    idade = Number(prompt("Digite a sua idade:"));
     let sexo = prompt("Digite o seu sexo: \n 1-Masculino \n 2-Feminino");
     let experiencia_m = prompt("Possui experiência na área? \n 1-Sim \n 2-Não");
+
     ///idade
     totalIdade += idade;
-    mIdade = totalIdade / totalCandidatos;
+    totalCandidatos++;
+
+    mIdade = totalIdade/totalCandidatos;
     //// sexo do candidato
     let msg_sexo = 0;
     if (sexo === "1"){
@@ -33,7 +36,6 @@ while(registro === "1"){
     }if (experiencia_m === "2"){
         msg_expe = "Não";
     }experiencia_m
-    totalCandidatos++;
     msg += "#############   ##########################   ##########################   #############" + "</br>";
     msg += "Nome: " + nome + "  ||   Idade: " + idade + "  ||  Sexo: " + msg_sexo + " ||  Número do candidato : " + num_candidato + "  ||  Possui Expêriencia:  " + msg_expe +"</br>";
     registro = prompt("Deseja registrar mais alguma pessoa? \n 1-Sim \n 2-Não");
@@ -41,9 +43,7 @@ while(registro === "1"){
 if (registro === "2") {
     alert("Você está deixando a área de registro. \n Aperte F5 para retornar ao início.");
   }
-
-  
 document.getElementById("candidatos_exp").innerHTML=totalExpe;
-document.getElementById("mIdade").innerHTML=mIdade.toFixed(2);
+document.getElementById("mIdade").innerHTML=mIdade;
 document.getElementById("candidato").innerHTML=msg;
 document.getElementById("totalCandidatos").innerHTML=totalCandidatos;
